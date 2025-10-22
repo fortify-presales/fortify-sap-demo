42dd0c96be2226f6c4747457eef9e2f123e86e60
kadraman <klee2@opentext.com>
2025-10-22 18:22:23 +0100

    Adding simple Fiori App

---
+++ src/z_ui5_fiori/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.fortify.demo.zui5fiori.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ src/z_ui5_fiori/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/fortify/demo/zui5fiori/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
